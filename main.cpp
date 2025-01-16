a551f3f37c7b285cbfb6f49a4f65aaa32a81eb71
fh.akramov <fh.akramov@newuu.uz>
2025-01-16 10:18:10 +0500

    name

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello World!" << endl;
+    cout << "How old are you" << endl;
+    cout <<  "Age:"<<endl;
+    cin>>age;
+    cout <<"YOUR AGE IS" << age<<endl;
+    return 0;
+}
