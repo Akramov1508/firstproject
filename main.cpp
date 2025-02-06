53a428e57cf37e4775d765296974a54ac47b6e63
fh.akramov <fh.akramov@newuu.uz>
2025-02-06 10:49:00 +0500

    lab4

---
+++ main.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // //double number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+    // if (number > 0) {
+    //     cout << "The number is positive." << endl;
+    // } else if (number < 0) {
+    //     cout << "The number is negative." << endl;
+    // } else {
+    //     cout << "The number is zero." << endl;
+    // }
+
+    //problem2
+    // //int number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+    // if (number % 2 == 0) {
+    //     cout << "The number is even." << endl;
+    // } else {
+    //     cout << "The number is odd." << endl;
+    // }
+
+    //probem3
+    // //int number1, number2;
+    // cout << "Enter number1: ";
+    // cin >> number1;
+    // cout << "Enter number2: ";
+    // cin >> number2;
+    // if (number1 > number2) {
+    //     cout <<  " Largest number is " << number1 << endl;
+    // } else if (number2 > number1) {
+    //     cout << " Largest number is " << number2 << endl;
+    // }
+
+    //problem4
+    // //int number1, number2, number3;
+    // cout<<"enter number1:";
+    // cin>>number1;
+    // cout<<"enter number2:";
+    // cin>>number2;
+    // cout<<"enter number3:";
+    // cin>>number3;
+    // if (number1>number2 && number1>number3) {
+    //     cout<<"largest number is "<<number1<<endl;
+    // }else if (number2>number1 && number2>number3) {
+    //     cout<<"largest number is "<<number2<<endl;
+    // }else if (number3>number1 && number3>number2) {
+    //     cout<<"largest number is "<<number3<<endl;
+    // }
+
+    //problem5
+    //int year;
+    // cout << "Enter a year: ";
+    // cin >> year;
+    // if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+    //     cout << year << "this is a leap year" << endl;
+    // }else {
+    //     cout << year << "this is not a leap year" << endl;
+    // }
+
+
+    //problem 6
+    // int v;
+    // cout<<"Enter the speed of a vehicle: ";
+    // cin>>v;
+    // if(v>80) {
+    // cout<<"Too fast"; }
+    // else if (v<20) {
+    //   cout<<"Too slow";
+    // }
+    // else { cout<<"Just right";}
+
+    //problem 7
+    // //int m;
+    // cout<< "enter your mark: ";
+    //  cin>>m;
+    // if(m>=50) {
+    //     cout<<"Pass";
+    // }
+    // else {
+    //     cout << "Fail";
+    // }
+
+
+    //problem 8
+    // //int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // if (n<0) {
+    //     cout << "Negative" << endl;
+    // }
+    // else if (n>0) {
+    //     cout << "Positive" << endl;
+    // }
+    // else {cout<< "Zero" << endl;}
+
+
+    //problem 9
+    //char c;
+    // cout << "Enter the command: ";
+    //  cin >> c;
+    //  if (c == 'g') {
+    //      cout<< "Go!";}
+    //  else if (c == 'y') {
+    //  cout<< "Get ready!";
+    //  }
+    //  else if (c == 'r') {
+    //  cout<<"Stop";
+    //  }
+
+
+    //problem 10
+    // int n;
+    // cout << "Enter your grade: ";
+    // cin >> n;
+    // switch (n / 10) {
+    //      case 10:
+    //      cout << "A";
+    //      break;
+    //     case 9:
+    //          cout << "A";
+    //     break;
+    //     case 8:
+    //         cout << "B";
+    //      break;
+    //     case 7:
+    //          cout << "C";
+    //     break;
+    //     case 6:
+    //         cout << "D";
+    //     break;
+    //     default:
+    //          cout << "F";
+    //     break;
+    //  }
+
+
+    //problem 11
+    // int a,b,c;
+    // cout<< "Enter 2 numbers: ";
+    // cin>>a>>b;
+    // c=a%b;
+    // if (c==0) {
+    //     cout<<a<<" is divisible by "<<b;
+    // }
+    // else {
+    //     cout<<a<<" is not divisible by "<<b;
+    // }
+
+
+    //problem 12
+    // int a,b,c,d;
+    // cout<<"Enter angels to form triangle: ";
+    // cin>>a>>b>>c;
+    // d=a+b+c;
+    // if (d<=180) {
+    //     cout<<"Triangle is valid";
+    // }
+    // else {
+    //     cout<<"Triangle is not valid";
+    // }
+
+
+    //problem 13
+    // char c;
+    // cout << "Enter a character: ";
+    // cin >> c;
+    // if (c >= 'a' && c <= 'z') {
+    //     cout<<"Lowercase alphabet";
+    // }
+    // else if (c >= 'A' && c <= 'Z') {
+    //     cout<<"Uppercase alphabet";
+    // }
+    // else {
+    //     cout<<"It is not an alphabet";// }
+
+
+    //problem 14
+    //int w1,p1,w2,p2;float pg1,pg2;
+    // cout<<"Enter the weight of first package: ";
+    // cin>>w1;
+    // cout<<"Enter the price of first package: ";
+    // cin>>p1;
+    // cout<<"Enter the weight of second package: ";
+    // cin>>w2;
+    // cout<<"Enter the price of second package: ";
+    // cin>>p2;
+    // pg1=w1/p1;
+    // pg2=w2/p2;
+    // if (w1>w2) {
+    //     cout<<"First package has better price"<<endl;
+    // }else if (w1<w2) {
+    //     cout<<"Second package has better price"<<endl;
+    // }
+    // else {
+    //     cout<<"Two packages have the same price";
+    // }
+
+
+    //problem 15
+    // int n,d1,d2,d3;
+    // cout<<"Enter three digit integer: ";
+    // cin>>n;
+    // d1=n%10;
+    // d3=n/100;
+    // if (d1==d3) {
+    //     cout<<n<<" is palindrome number"<<endl;
+    // }
+    // else {
+    //     cout<<n<<" is not palindrome number"<<endl;
+    // }
+
+
+    //problem 16
+    //int x,y;
+    // float d;
+    // cout<<"Enter x,y coordinates (in correcr order): ";
+    // cin>>x>>y;
+    // d=((x-0)^2+(y-0)^2)^(1/2);
+    // if(d<=10) {
+    //     cout<< "Your coordinate is in circle";
+    // }else if(d>10) {
+    //     cout<<"Your coordinate is not in circle";}
+
+    //problem17
+    // float gpa;
+    // cout << "Enter your GPA: ";
+    // cin >> gpa;
+    // int category = (gpa >= 4.0) ? 4 : (gpa >= 3.5) ? 3 : (gpa >= 3.0) ? 2 : 1;
+    // switch (category) {
+    //     case 4:
+    //     cout << "You got 80% scholarship" << endl;
+    //     break;
+    //     case 3:        cout << "You got 60% scholarship" << endl;
+    //     break;
+    //     case 2:
+    //         cout << "You got 50% scholarship" << endl;
+    //     break;
+    //     default:
+    //         cout << "No scholarship" << endl;
+    // }
+
+
+
+
+
+
+
+
+    return 0;
+
+}
+
